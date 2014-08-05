b1a272f3f60dfd1b846c96f18f12f23454eee867
giusepped <giusdesan@gmail.com>
2014-08-05 18:34:04 +0100

    Update PromissoryNote.java

--- Week7/PromissoryNote/PromissoryNote.java
+++ Week7/PromissoryNote/PromissoryNote.java
@@ -1,28 +1,30 @@
 
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author giuseppedesantis
  */
 public class PromissoryNote {
-  private HashMap<String, double> loans;
+    
+    private HashMap<String, Double> loans;
   
   
-  public PromissoryNote(){
-    this.loans = new HashMap<String, double>();
-  }
-  
-  public void setLoan(String toWhom, double value){
-    this.loans.put(toWhom, value);
-  }
-  
-  public double howMuchIsTheDebt(String whose){
-    if(this.loans.containsKey(whose){
-      return this.loans.get(whose);
+    public PromissoryNote(){
+        this.loans = new HashMap<String, Double>();
     }
+  
+    public void setLoan(String toWhom, double value){
+        this.loans.put(toWhom, value);
+    }
+  
+    public double howMuchIsTheDebt(String whose){
+        if(this.loans.containsKey(whose)){
+            return this.loans.get(whose);
+        }
     
     return 0;
-  }
-
+    }
 
 }
