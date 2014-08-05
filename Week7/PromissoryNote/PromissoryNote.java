840adbb87877dbaee9842a2a95e18316c9cc54cd
giusepped <giusdesan@gmail.com>
2014-08-05 16:43:07 +0100

    Update PromissoryNote.java

--- Week7/PromissoryNote/PromissoryNote.java
+++ Week7/PromissoryNote/PromissoryNote.java
@@ -1,10 +1,28 @@
-import java.util.Scanner;
+
 
 /**
  *
  * @author giuseppedesantis
  */
 public class PromissoryNote {
+  private HashMap<String, double> loans;
+  
+  
+  public PromissoryNote(){
+    this.loans = new HashMap<String, double>();
+  }
+  
+  public void setLoan(String toWhom, double value){
+    this.loans.put(toWhom, value);
+  }
+  
+  public double howMuchIsTheDebt(String whose){
+    if(this.loans.containsKey(whose){
+      return this.loans.get(whose);
+    }
+    
+    return 0;
+  }
 
 
 }
