ce2400e61554fb3ab6e66e925445863c4d3c3980
giusepped <giusdesan@gmail.com>
2014-08-06 00:01:50 +0100

    Update Dictionary.java

--- Week7/Dictionary/Dictionary.java
+++ Week7/Dictionary/Dictionary.java
@@ -1,8 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+
 /**
  *
  * @author giuseppedesantis
  */
- 
 public class Dictionary {
-
+    private HashMap<String, String> dictionary;
+    
+    public Dictionary(){
+        this.dictionary = new HashMap<String, String>();
+    }
+    
+    public String translate(String word){
+        if(this.dictionary.containsKey(word)){
+            return this.dictionary.get(word);
+        }
+        return null;
+    }
+    
+    public void add(String word, String translation){
+        this.dictionary.put(word, translation);
+    }
+    
+    public int amountOfWords(){
+        return this.dictionary.size();
+    }
+    
+    public ArrayList<String> translationList(){
+        ArrayList<String> translationList = new ArrayList<String>();
+        for(String key : this.dictionary.keySet()){
+            String translation = key + " = " + this.translate(key);
+            translationList.add(translation);
+        }
+        return translationList;
+    }
+    
 }
+
