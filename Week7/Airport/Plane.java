9f98e73b79af78f1a8f995530d1eed61f5e1bbbb
giusepped <giusdesan@gmail.com>
2014-08-06 11:49:33 +0100

    Create Plane.java

---
+++ Week7/Airport/Plane.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author giuseppedesantis
+ */
+ 
+ 
+public class Plane {
+  private String id;
+  private int capacity;
+  
+    public Plane(String id, int capacity){
+      this.id = id;
+      this.capacity = capacity;
+    }
+}
