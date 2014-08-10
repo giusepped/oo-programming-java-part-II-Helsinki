a0b80c71c0f9cb59cf9c7aa346d3e428c7349802
giusepped <giusdesan@gmail.com>
2014-08-10 22:37:10 +0100

    Update FlightDatabase.java

--- Week7/Airport/FlightDatabase.java
+++ Week7/Airport/FlightDatabase.java
@@ -1,3 +1,4 @@
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.List;
 public class FlightDatabase {
     private ArrayList<Plane> planes;
     private ArrayList<Flight> flights;
-    private HashMap<Plane, ArrayList<Flight>> database;
+    private Map<Plane, ArrayList<Flight>> database;
     
     public FlightDatabase(){
         this.planes = new ArrayList<Plane>();
@@ -19,8 +20,10 @@ public class FlightDatabase {
     }
     
     public void addPlane(String id, int capacity){
+        ArrayList<Flight> init = new ArrayList<Flight>();
         Plane plane = new Plane(id, capacity);
         this.planes.add(plane);
+        this.database.put(plane, init);
     }
     
     public void addFlight(String id, String depart, String arriv){
@@ -31,8 +34,7 @@ public class FlightDatabase {
             }
         }
         Flight flight = new Flight(depart, arriv);
-        this.flights.add(flight);
-        this.database.put(plane, flights);
+        this.database.get(plane).add(flight);
     }
     
     public void printPlanes(){
@@ -42,10 +44,15 @@ public class FlightDatabase {
     }
     
     public void printFlights(){
-        
+        for(Plane p : this.database.keySet()){
+            ArrayList<Flight> flights = this.database.get(p);
+            for(Flight f : flights){
+                System.out.println(p + " " + f);
+            }
+        }
     }
     
-    public void printOnePlane(String id){
+    public void printPlaneInfo(String id){
         for(Plane p : this.planes){
             if(p.getPlane().equals(id)){
                 System.out.println(p);
