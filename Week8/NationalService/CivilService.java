49918bd751595301742d414f7694fb5ad069211a
giusepped <giusdesan@gmail.com>
2014-08-14 16:30:48 +0100

    Create CivilService.java

---
+++ Week8/NationalService/CivilService.java
@@ -0,0 +1,18 @@
+public class CivilService implements NationalService{
+  private int daysLeft;
+  
+  public CivilService(){
+    this.daysLeft = 362;
+    }
+  
+  public int getDaysLeft(){
+    return this.daysLeft;
+    }
+    
+  public void work(){
+    if(this.daysLeft >= 1){
+      this.daysLeft--;
+      }
+    }
+    
+}
