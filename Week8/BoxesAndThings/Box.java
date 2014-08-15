0892bbc18dac38632a95cc12cef1939e59b6d2a6
giusepped <giusdesan@gmail.com>
2014-08-15 13:38:25 +0100

    Update Box.java

--- Week8/BoxesAndThings/Box.java
+++ Week8/BoxesAndThings/Box.java
@@ -1,9 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  *
  * @author giuseppedesantis
  */
 
-public class Box {
+public class Box implements ToBeStored{
+    private ArrayList<ToBeStored> things;
+    private double maximumWeight;
     
+    public class Box(double maximumWeight){
+        this.maximumWeight = maximumWeight;
+        this.things = new ArrayList<ToBeStored>();
+    }
+    
+    public void addThing(ToBeStored thing){
+        if(this.weight() + thing.weight() < maximumWeight){
+            this.things.add(thing);
+        }
+    }
+    
+    public double weight(){
+        double weight = 0;
+        for(ToBeStored thing : this.things){
+            weight =+ thing;
+        }
+        return weight;
+    }
+    
+    @Override
+    public void toString(){
+        return "Box: " + this.things.size() + " things, total weight " + this.weight() + " kg";
+    }
 }
