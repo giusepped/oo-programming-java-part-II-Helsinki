7983deee37254a571a1815698cf2d55e22ee5d96
giusepped <giusdesan@gmail.com>
2014-08-15 11:47:01 +0100

    Update Book.java

--- Week8/BoxesAndThings/Book.java
+++ Week8/BoxesAndThings/Book.java
@@ -4,6 +4,19 @@
  * @author giuseppedesantis
  */
 
-public class Book {
+public class Book implements ToBeStored{
+    private String writer;
+    private String name;
+    private double weight;
+    
+    public Book(String writer, String name, double weight){
+        this.writer = writer;
+        this.name = name;
+        this.weight = weight;
+    }
+    
+    public double weight(){
+        return this.weight;
+    }
     
 }
