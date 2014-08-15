b9ea53e1e29aa2c309ddf4ff1849070ce24b63b7
giusepped <giusdesan@gmail.com>
2014-08-15 12:13:19 +0100

    Update CD.java

--- Week8/BoxesAndThings/CD.java
+++ Week8/BoxesAndThings/CD.java
@@ -4,6 +4,20 @@
  * @author giuseppedesantis
  */
 
-public class CD {
+public class CD implements ToBeStored{
+    private String artist;
+    private String title;
+    private int publishingYear;
+    private double weight;
     
+    public CD(String artist, String title, int publishingYear){
+        this.artist = artist;
+        this.title = title;
+        this.publishingYear = publishingYear;
+        this.weight = 0.1;
+    }
+    
+    public double weight(){
+        return this.weight;
+    }
 }
