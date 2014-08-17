49336117a7f5f4da1fee7516313b118982aa9a42
giusepped <giusdesan@gmail.com>
2014-08-17 22:40:52 +0100

    Create TextUserInterface.java

---
+++ Week9/FirstPackages/mooc.ui/TextUserInterface.java
@@ -0,0 +1,13 @@
+
+package mooc.ui;
+
+/**
+ *
+ * @author giuseppedesantis
+ */
+public class TextUserInterface implements UserInterface{
+    @Override
+    public void update(){
+        System.out.println("Updating the user interface");
+    }
+}
