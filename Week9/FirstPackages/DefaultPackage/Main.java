4e411bb2c160eaab637972151c8c381b83f42b8d
giusepped <giusdesan@gmail.com>
2014-08-17 22:39:43 +0100

    Create Main.java

---
+++ Week9/FirstPackages/DefaultPackage/Main.java
@@ -0,0 +1,11 @@
+import mooc.logic.ApplicationLogic;
+import mooc.ui.UserInterface;
+import mooc.ui.TextUserInterface;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterface ui = new TextUserInterface();
+        new ApplicationLogic(ui).execute(3);
+    }
+}
