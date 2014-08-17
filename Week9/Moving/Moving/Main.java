0304fe9b603337d5bee9e54be6648c74b5fc14d5
giusepped <giusdesan@gmail.com>
2014-08-17 23:23:53 +0100

    Create Main.java

---
+++ Week9/Moving/Moving/Main.java
@@ -0,0 +1,33 @@
+package moving;
+
+import moving.domain.Item;
+import moving.domain.Thing;
+import moving.domain.Box;
+import moving.logic.Packer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test your program here
+        List<Thing> things = new ArrayList<Thing>();
+        things.add(new Item("passport", 2));
+        things.add(new Item("toothbrash", 1));
+        things.add(new Item("book", 4));
+        things.add(new Item("circular saw", 8));
+
+        // we create a packer which uses boxes whose valume is 10
+        Packer packer = new Packer(10);
+
+        // we ask our packer to pack things into boxes
+        List<Box> boxes = packer.packThings( things );
+
+        System.out.println("number of boxes: "+boxes.size());
+
+        for (Box box : boxes) {
+            System.out.println("  things in the box: "+box.getVolume()+" dm^3");
+        }
+    }
+}
