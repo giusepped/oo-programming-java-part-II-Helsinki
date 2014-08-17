dddceec4444d8bb7a6fce1efdc997f03e0fdebba
giusepped <giusdesan@gmail.com>
2014-08-17 12:36:35 +0100

    Create Shop.java

---
+++ Week8/OnlineShop/Shop.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Shop {
+
+    private Storehouse store;
+    private Scanner reader;
+
+    public Shop(Storehouse store, Scanner reader) {
+        this.store = store;
+        this.reader = reader;
+    }
+
+    // the method to deal with a customer in the shop
+    public void manage(String customer) {
+        ShoppingBasket basket = new ShoppingBasket();
+        System.out.println("Welcome to our shop " + customer);
+        System.out.println("below is our sale offer:");
+
+        for (String product : store.products()) {
+            System.out.println( product );
+        }
+
+        while (true) {
+            System.out.print("what do you want to buy (press enter to pay):");
+            String product = reader.nextLine();
+            if (product.isEmpty()) {
+                break;
+            }
+
+            // here, you write the code to add a product to the shopping basket, if the storehouse is not empty
+            // and decreases the storehouse stocks
+            // do not touch the rest of the code!
+            store.take(product);
+            int price = store.price(product);
+            for(String p : store.products()){
+                if(p.equals(product)){
+                    basket.add(product, price);
+                }
+            }
+
+        }
+
+        System.out.println("your purchases are:");
+        basket.print();
+        System.out.println("basket price: " + basket.price());
+    }
+}
+     
