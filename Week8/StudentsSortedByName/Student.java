86cb1742a3ead1d978a5b75b1de21d1c395b34ff
giusepped <giusdesan@gmail.com>
2014-08-17 13:18:25 +0100

    Create Student.java

---
+++ Week8/StudentsSortedByName/Student.java
@@ -0,0 +1,23 @@
+public class Student implements Comparable<Student>{
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    @Override
+    public int compareTo(Student student){
+        return this.name.compareToIgnoreCase(student.name);
+    }
+
+}
