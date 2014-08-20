1a40b0ad1fcc76aa860279616a53c0bfeaa9df5f
giusepped <giusdesan@gmail.com>
2014-08-20 21:02:27 +0100

    Update Calculator.java

--- Week9/MethodArgumentValidation/Validation/Calculator.java
+++ Week9/MethodArgumentValidation/Validation/Calculator.java
@@ -3,7 +3,9 @@ package validation;
 public class Calculator {
 
     public int multiplication(int fromInteger) {
-
+        if(fromInteger < 0){
+            throw new IllegalArgumentException("negative numbers are not accepted");
+        }
         int multiplication = 1;
         for (int i = 1; i <= fromInteger; i++) {
             multiplication *= i;
@@ -13,7 +15,9 @@ public class Calculator {
     }
 
     public int binomialCoefficient(int setSize, int subsetSize) {
-
+        if(setSize < 0 || subsetSize < 0 || subsetSize > setSize){
+            throw new IllegalArgumentException("invalid entries");
+        }
         int numerator = multiplication(setSize);
         int denominator = multiplication(subsetSize) * multiplication(setSize - subsetSize);
 
