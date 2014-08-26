ea4104f7c10c3a876fdaad885c57d96bc20b23c4
giusepped <giusdesan@gmail.com>
2014-08-26 21:57:25 +0100

    Create ProductContainerRecorder.java

---
+++ week10/Container/containers/ProductContainerRecorder.java
@@ -0,0 +1,14 @@
+
+package containers;
+
+/**
+ *
+ * @author giuseppedesantis
+ */
+public class ProductContainerRecorder extends ContainerHistory{
+    
+    
+    public ProductContainerRecorder(String productName, double capacity, double initialVolume) {
+        
+    }
+}
