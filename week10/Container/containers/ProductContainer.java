5bdea327fdd1aa24e01373cfae0ab07b49be13ac
giusepped <giusdesan@gmail.com>
2014-08-26 21:57:02 +0100

    Update ProductContainer.java

--- week10/Container/containers/ProductContainer.java
+++ week10/Container/containers/ProductContainer.java
@@ -1,60 +1,29 @@
+
 package containers;
 
-public class Container {
-
-    private double capacity;
-    private double volume;
-
-    public Container(double tilavuus) {
-        if (tilavuus > 0.0) {
-            this.capacity = tilavuus;
-        } else {
-            this.capacity = 0.0;
-        }
-
-        volume = 0.0;
+/**
+ *
+ * @author giuseppedesantis
+ */
+public class ProductContainer extends Container{
+    
+    private String productName;
+    
+    public ProductContainer(String productName, double capacity){
+        super(capacity);
+        this.productName = productName;
     }
-
-
-    public double getVolume() {
-        return volume;
+    
+    public String getName(){
+        return this.productName;
     }
-
-    public double getOriginalCapacity() {
-        return capacity;
+    
+    public void setName(String newName){
+        this.productName = newName;
     }
-
-    public double getCurrentCapacity() {
-        return capacity - volume;
-    }
-
-    public void addToTheContainer(double amount) {
-        if (amount < 0) {
-            return;
-        }
-        if (amount <= getCurrentCapacity()) {
-            volume = volume + amount;
-        } else {
-            volume = capacity;
-        }
-    }
-
-    public double takeFromTheContainer(double amount) {
-        if (amount < 0) {
-            return 0.0;
-        }
-        if (amount > volume) {
-            double everything = volume;
-            volume = 0.0;
-            return everything;
-        }
-
-        volume = volume - amount;
-        return amount;
-    }
-
+    
     @Override
-    public String toString() {
-        return "volume = " + volume + ", free space " + getCurrentCapacity();
+    public String toString(){
+        return this.productName + ": " + super.toString();
     }
 }
