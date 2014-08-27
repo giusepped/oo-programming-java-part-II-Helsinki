e92be2489e94941eedf8f7270f0c9aa4e86af2d0
giusepped <giusdesan@gmail.com>
2014-08-27 07:07:51 +0100

    Create Person.java

---
+++ week10/PersonAndTheirHeirs/people/Person.java
@@ -0,0 +1,21 @@
+
+package people;
+
+/**
+ *
+ * @author giuseppedesantis
+ */
+public class Person {
+    private String name;
+    private String address;
+    
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+    
+    @Override
+    public String toString(){
+        return this.name + "\n  " + this.address;
+    }
+}
