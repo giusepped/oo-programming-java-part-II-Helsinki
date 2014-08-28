a46ed5e54128b0d6330153845234ecfa7031aa5e
giusepped <giusdesan@gmail.com>
2014-08-28 10:33:23 +0100

    Update BulkTank.java

--- week10/FarmSimulator/farmsimulator/BulkTank.java
+++ week10/FarmSimulator/farmsimulator/BulkTank.java
@@ -26,4 +26,30 @@ public class BulkTank {
     public double getVolume(){
         return this.volume;
     }
+    
+    public double howMuchFreeSpace(){
+        return this.capacity - this.volume;
+    }
+    
+    public void addToTank(double amount){
+        if(amount <= this.howMuchFreeSpace()){
+            this.volume += amount;
+        }else{
+            this.volume = this.capacity;
+        }
+    }
+    
+    public double takeFromTank(double amount){
+        if(amount >= this.volume){
+            this.volume = 0;
+            return this.volume;
+        }else{
+            return this.volume - amount;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        
+    }
 }
