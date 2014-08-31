78823326b3a7ebd8d2003f916b3c863c29d20f6e
giusepped <giusdesan@gmail.com>
2014-08-31 22:52:03 +0100

    Update MilkingRobot.java

--- week10/FarmSimulator/farmsimulator/MilkingRobot.java
+++ week10/FarmSimulator/farmsimulator/MilkingRobot.java
@@ -6,11 +6,29 @@ package farmsimulator;
  * @author giuseppedesantis
  */
 public class MilkingRobot {
-    
+    private BulkTank bulkTank;
     
     public MilkingRobot(){
         
     }
     
+    public BulkTank getBulkTank(){
+        if(this.bulkTank == null){
+            return null;
+        }else{
+            return this.bulkTank;
+        }
+    }
     
+    void setBulkTank(BulkTank tank){
+        this.bulkTank = tank;
+    }
+    
+    void milk(Milkable milkable){
+        if(this.getBulkTank() == null){
+            throw new IllegalStateException("The MilkingRobot hasn't been installed");
+        }else{
+            
+        }
+    }
 }
