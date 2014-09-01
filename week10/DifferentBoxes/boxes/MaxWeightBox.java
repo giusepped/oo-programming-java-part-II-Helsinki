3221958176edc9872f9b2430f69c52756aab2c6f
giusepped <giusdesan@gmail.com>
2014-09-01 18:22:26 +0100

    Update MaxWeightBox.java

--- week10/DifferentBoxes/boxes/MaxWeightBox.java
+++ week10/DifferentBoxes/boxes/MaxWeightBox.java
@@ -1,13 +1,40 @@
+
 package boxes;
 
-import java.util.Collection;
+/**
+ *
+ * @author giuseppedesantis
+ */
 
-Public class MaxWeightBox extends Box{
-  private int maxWeight;
-  private int currentWeight;
-  
-  public MaxWeightBox(int maxWeight){
-  }
-  
-  
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class MaxWeightBox extends Box{
+    private int maxWeight;
+    private int currentWeight;
+    private Collection<Thing> things;
+    
+    public MaxWeightBox(int maxWeight){
+        this.maxWeight = maxWeight;
+        this.currentWeight = 0;
+        this.things = new LinkedList<Thing>();
+    }
+    
+    @Override
+    public void add(Thing thing){
+        if(thing.getWeight() + this.currentWeight <= this.maxWeight){
+            this.things.add(thing);
+            this.currentWeight += thing.getWeight();
+        }
+    }
+    
+    @Override
+    public boolean isInTheBox(Thing thing){
+        for(Thing t : this.things){
+            if(t.equals(thing)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
