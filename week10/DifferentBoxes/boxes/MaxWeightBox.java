f9dd06eece11c74d9e03ede1d6fcc83e851c315b
giusepped <giusdesan@gmail.com>
2014-09-01 13:09:01 +0100

    Create MaxWeightBox.java

---
+++ week10/DifferentBoxes/boxes/MaxWeightBox.java
@@ -0,0 +1,11 @@
+package boxes;
+
+import java.util.Collection;
+
+Public class MaxWeightBox extends Box{
+  private int maxWeight;
+  private int currentWeight;
+  
+  public MaxWeightBox(){
+  }
+}
