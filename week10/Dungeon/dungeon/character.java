1281a33d853a47b710922547f3fcfd2245bb8811
giusepped <giusdesan@gmail.com>
2014-09-02 16:19:34 +0100

    Create character.java

---
+++ week10/Dungeon/dungeon/character.java
@@ -0,0 +1,16 @@
+package dungeon;
+
+public abstract class Character{
+  
+  private String name;
+  private int x;
+  private int y;
+  
+  public Character(String name, int x, int y){
+    this.name = name;
+    this.x = x;
+    this.y = y;
+  }
+  
+  
+}
