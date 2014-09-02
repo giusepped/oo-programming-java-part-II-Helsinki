d1649b7c7844601806846b12b85afd7e1faa776a
giusepped <giusdesan@gmail.com>
2014-09-02 16:34:44 +0100

    Update and rename character.java to Character.java

--- week10/Dungeon/dungeon/Character.java
+++ week10/Dungeon/dungeon/Character.java
@@ -3,13 +3,9 @@ package dungeon;
 public abstract class Character{
   
   private String name;
-  private int x;
-  private int y;
   
   public Character(String name, int x, int y){
     this.name = name;
-    this.x = x;
-    this.y = y;
   }
   
   
