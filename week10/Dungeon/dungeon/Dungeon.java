dff3d490ba432d47b5ac301ec0442aa4afbe1b31
giusepped <giusdesan@gmail.com>
2014-09-04 21:35:50 +0100

    Create Dungeon.java

---
+++ week10/Dungeon/dungeon/Dungeon.java
@@ -0,0 +1,18 @@
+
+package dungeon;
+
+/**
+ *
+ * @author giuseppedesantis
+ */
+public class Dungeon {
+    
+    
+    public Dungeon(int length, int height, int vampires, int moves, boolean vampiresMove){
+        
+    }
+    
+    public void Run(){
+        
+    }
+}
