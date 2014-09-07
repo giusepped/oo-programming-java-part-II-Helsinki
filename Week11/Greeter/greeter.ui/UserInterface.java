0b8908b41a2fce4886abbe005ce7c0cd0626d985
giusepped <giusdesan@gmail.com>
2014-09-07 18:20:29 +0100

    Create UserInterface.java

---
+++ Week11/Greeter/greeter.ui/UserInterface.java
@@ -0,0 +1,35 @@
+package greeter.ui;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+public class UserInterface implements Runnable {
+
+    private JFrame frame;
+
+    @Override
+    public void run() {
+        frame = new JFrame("Swing on");
+        frame.setPreferredSize(new Dimension(400, 200));
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        createComponents(frame.getContentPane());
+        
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+
+    private void createComponents(Container container) {
+        JLabel text = new JLabel("Hi!");
+        container.add(text);
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+}
